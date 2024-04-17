b74c6f46b508cdb79f9f90988e9461cddfd128d0
unknown <ahamedhuzair13@gmail.com>
2024-04-17 17:03:18 +0530

    Day 7 Task Completed

---
+++ Day7/RequestTrackerAppSolution/RequestTrackerBLLibrary/DepartmentBL.cs
@@ -0,0 +1,20 @@
+﻿using RequestTrackerAppModelLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using RequestTrackerDALLibrary;
+namespace RequestTrackerBLLibrary
+{
+    public class DepartmentBL
+    {
+        readonly IRepository<int, Department> _departmentRepository;
+        public DepartmentBL()
+        {
+            _departmentRepository = new DepartmentRepository();
+        }
+
+    }
+}
---
+++ Day7/RequestTrackerAppSolution/RequestTrackerBLLibrary/IDepartmentService.cs
@@ -0,0 +1,22 @@
+﻿using RequestTrackerAppModelLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using RequestTrackerDALLibrary;
+;
+namespace RequestTrackerBLLibrary
+{
+    public interface IDepartmentService
+    {
+        int AddDepartment(DepartmentBL department);
+        DepartmentBL ChangeDepartmentName(string departmentOldName, string departmentNewName);
+        DepartmentBL GetDepartmentById(int id);
+        DepartmentBL GetDepartmentByName(string departmentName);
+        int GetDepartmentHeadId(int departmentId);
+
+
+    }
+}
