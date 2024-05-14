84b6887e4f0b213591d4664edecc7117ff246e98
unknown <ahamedhuzair13@gmail.com>
2024-05-14 16:19:42 +0530

    Day 24 task To do a Web App for Clinic Appointment and use GET, PUT, and POST completed

---
+++ Day24/ClinicAppointmentAPISolution/ClinicAppointmentAPI/Interfaces/IDoctorServices.cs
@@ -0,0 +1,11 @@
+﻿using ClinicAppointmentAPI.Models;
+
+namespace ClinicAppointmentAPI.Interfaces
+{
+    public interface IDoctorServices
+    {
+        public Task<IEnumerable<Doctor>> GetDoctors();
+        public Task<Doctor> UpdateDoctorExperience(int doctorID,double experience);
+        public Task<IEnumerable<Doctor>> GetDoctorBySpecilization(string specialization);
+    }
+}
---
+++ Day24/ClinicAppointmentAPISolution/ClinicAppointmentAPI/Repositories/DoctorRepository.cs
@@ -0,0 +1,62 @@
+﻿using ClinicAppointmentAPI.contexts;
+using ClinicAppointmentAPI.Exceptions;
+using ClinicAppointmentAPI.Interfaces;
+using ClinicAppointmentAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ClinicAppointmentAPI.Repositories
+{
+    public class DoctorRepository : IRepository<int, Doctor>
+    {
+        public readonly ClinicAppointmentContext _context;
+
+        public DoctorRepository(ClinicAppointmentContext context)
+        {
+            _context = context;
+        }
+
+
+        public async Task<Doctor> Add(Doctor item)
+        {
+            _context.Add(item);
+            await _context.SaveChangesAsync();
+            return item;
+        }
+
+        public async Task<Doctor> Delete(int key)
+        {
+            var doctor = await Get(key);
+            if (doctor != null)
+            {
+                _context.Remove(doctor);
+                await _context.SaveChangesAsync(true);
+                return doctor;
+            }
+            throw new NoSuchDoctorFoundException();
+        }
+
+        public async Task<Doctor> Get(int key)
+        {
+            var doctors = await _context.Doctors.FirstOrDefaultAsync(e => e.Id == key);
+            return doctors;
+        }
+
+        public async Task<IEnumerable<Doctor>> Get()
+        {
+            var doctors = await _context.Doctors.ToListAsync();
+            return doctors;
+        }
+
+        public async Task<Doctor> Update(Doctor item)
+        {
+            var doctor = await Get(item.Id);
+            if (doctor != null)
+            {
+                _context.Update(item);
+                await _context.SaveChangesAsync(true);
+                return doctor;
+            }
+            throw new NoSuchDoctorFoundException();
+        }
+    }
+}
