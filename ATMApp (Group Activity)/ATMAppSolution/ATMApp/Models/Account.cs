ec0345c6971ee31b3f74edcad2641ad28791039f
unknown <ahamedhuzair13@gmail.com>
2024-07-11 10:51:03 +0530

    Group Activity - ATM

---
+++ ATMApp (Group Activity)/ATMAppSolution/ATMApp/Models/Account.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace ATMApp.Models
+{
+    public class Account
+    {
+        [Key]
+        public int AccountId { get; set; }
+
+        public decimal Balance { get; set; }
+    }
+}
